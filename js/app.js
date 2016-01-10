7580daa243f4abeb660cfef29591d4548d46e12c
Joe Fearnley <joe.fearnley@gmail.com>
2016-01-10 16:42:02 -0500

    Adding first parts of service to look up city on keyup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 (function() {
 	var app = angular.module('pizzaPlease', []);
 
-	//app.controller('CitySearchController', function() {});
-});
+	app.controller('SearchCityController', function($scope) {
+		$scope.service = cityService;
+	});
+
+	app.service('cityService', function($http) {
+		var url = "http://gd.geobytes.com/AutoCompleteCity?callback=?&filter=USCA&template=<geobytes%20city>,%20<geobytes%20code>&q=";
+
+		
+	});
+
+})();
