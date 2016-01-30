15c0260d106f289b64814926e5b69c40a01e56ec
Joe Fearnley <joe.fearnley@gmail.com>
2016-01-30 12:52:01 -0500

    Adding google places autocomplete to search box

--- js/app.js
+++ js/app.js
@@ -1,35 +1,54 @@
 
+
 (function() {
-	var app = angular.module('pizzaPlease', ['ui.bootstrap']);
+	var app = angular.module('pizzaPlease', []);
 
 	app.controller('SearchCityController', function($scope, $http) {
 
 		//$scope.service = cityService;
 		$scope.title = 'Find Pizza';
 
-		$http.get('airplanes.json').success(function(data) {
-			$scope.airplanes = data;
-		});
+		// $http.get('airplanes.json').success(function(data) {
+		// 	$scope.airplanes = data;
+		// });
+
+		// $scope.gPlace;
+
+		$scope.findPizza = function() {
+			console.log('Finding pizza');
+
+		// 	$http({
+		// 		method: 'JSONP',
+		// 		url: weatherUrl + $scope.city
+		// }).then(function(data) {
+		// 	console.log(data);
+		// });
+
+		};
+
 	});
 
-	// app.service('cityService', function($http) {
-	// 	//var url = 'http://gd.geobytes.com/AutoCompleteCity?callback=?&filter=USCA&template=<geobytes%20city>,%20<geobytes%20code>&q=';
-	// });
+	app.directive('googleplace', function() {
+	    return {
+	        require: 'ngModel',
+	        scope: {
+	            ngModel: '=',
+	            details: '=?'
+	        },
+	        link: function(scope, element, attrs, model) {
+	            var options = {
+					types: ['(cities)'],
+					componentRestrictions: {country: 'us'}
+	            };
+	            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
-	// define factory for data source
-	// myApp.factory('States', function(){
-	// 	var states = [
-	// 		"Alabama", "Alaska", "Arizona", "Arkansas", "California",
-	// 		"Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
-	// 		"Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
-	// 		"Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
-	// 		"Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
-	// 		"Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
-	// 		"New York", "North Dakota", "North Carolina", "Ohio", "Oklahoma",
-	// 		"Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
-	// 		"South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
-	// 		"Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-	// 	];
-	// 	return states;
-	// });
+	            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+	                scope.$apply(function() {
+	                    scope.details = scope.gPlace.getPlace();
+	                    model.$setViewValue(element.val());
+	                });
+	            });
+	        }
+	    };
+	});
 })();
