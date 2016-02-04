abf1d763ea15e65e76e5af233dbff9ce3f6a1b0e
Joe Fearnley <joe.fearnley@gmail.com>
2016-02-04 08:37:07 -0500

    Changing the way the server port is grabbed

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.get('/search', function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log('Example app listening on port 3000!');
 });
