7339627e9f1ab9d0a8a209b6b58213045beac841
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-25 10:38:29 -0500

    Removing bower and original jasmine libraries

---
+++ js/tests/app.spec.js
@@ -0,0 +1,5 @@
+describe('App loaded and is working', function() {
+    it('loaded', function() {
+        expect(true).toBeTrue();
+    });
+});
---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'js/app.js',
+            'js/tests/*.js'
+        ],
+        exclude: [
+        ],
+        preprocessors: {
+        },
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['PhantomJS'],
+        singleRun: false,
+        concurrency: Infinity
+    });
+}
