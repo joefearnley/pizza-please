2fe08baa287a3518970a05aaa72c4cd09d331228
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-28 21:51:11 -0500

    Attempting to mock http request. Requst denied

--- js/tests/app.spec.js
+++ js/tests/app.spec.js
@@ -1,6 +1,7 @@
 describe('Pizza Please App Test Suite', function() {
     var searchController;
     var scope;
+    var $httpBackend;
 
     describe('App loaded and is working', function() {
         it('loaded', function() {
@@ -12,10 +13,14 @@ describe('Pizza Please App Test Suite', function() {
 
         beforeEach(angular.mock.module('pizzaPlease'));
 
-        beforeEach(angular.mock.inject(function(_$controller_, $rootScope){
+        beforeEach(angular.mock.inject(function(_$controller_, $rootScope) {
             scope = $rootScope.$new();
             scope.city = 'Norton Shores, MI';
             searchController = _$controller_('SearchController', { $scope: scope });
+        }));
+
+        beforeEach(inject(function($injector) {
+            $httpBackend = $injector.get('$httpBackend');
         }));
 
         it('should exist', function() {
@@ -53,11 +58,33 @@ describe('Pizza Please App Test Suite', function() {
             });
 
             it('should find pizza locations', function() {
+
+                var fakeResponse = {
+                    success: true,
+                    error: null,
+                    locations: [
+                        {
+                            name: "Mr Scrib's Pizza",
+                            phone: "2317331857",
+                            location: {
+                                city: "Muskegon",
+                                display_address: [
+                                    "3044 Henry St",
+                                    "Muskegon, MI 49441"
+                                ]
+                            }
+                        }
+                    ]
+                };
+
+                // $httpBackend.when('GET', '/search')
+                //     .respond(200, fakeResponse);
+
                 scope.findPizza();
 
                 // should have locations
-                expect(scope.locations).toBeDefined();
-                //expect(scope.locations.length).toBe(20);
+                //expect(fakeResponse.locations).toBeDefined();
+                // expect(scope.locations.length).toBe(1);
 
                 // should have a map
                 //expect(scope.locations.length).toBe(20);
@@ -74,27 +101,49 @@ describe('Pizza Please App Test Suite', function() {
 });
 
 describe('Search Service', function() {
-    var LocationService ;
+    var LocationService;
+    var $httpBackend;
 
     beforeEach(angular.mock.module('pizzaPlease'));
 
-    beforeEach(inject(function(_SearchService_) {
+    beforeEach(inject(function(_SearchService_, $rootScope, _$httpBackend_) {
         SearchService = _SearchService_;
+        $httpBackend = _$httpBackend_;
     }));
 
     it('should exist', function() {
         expect(SearchService).toBeDefined();
     });
 
-    describe('seach', function() {
+    describe('search', function() {
 
-        var locations;
+        var fakeResponse = {
+            success: true,
+            error: null,
+            locations: [
+                {
+                    name: "Mr Scrib's Pizza",
+                    phone: "2317331857",
+                    location: {
+                        city: "Muskegon",
+                        display_address: [
+                            "3044 Henry St",
+                            "Muskegon, MI 49441"
+                        ]
+                    }
+                }
+            ]
+        };
+
+        $httpBackend.when('GET', '/search')
+            .respond(200, fakeResponse);
 
         it('should find locations', function() {
+            $httpBackend.flush();
+
             locations = SearchService.search();
 
-            // should have locations (length greateer than 1)''
-            // legnth should be 20?
+            expect(location.length).toBe(1);
         });
     });
 
