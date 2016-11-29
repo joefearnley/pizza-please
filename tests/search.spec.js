ba2a8b7f8c245f96f0f0f165043215e495cfc88b
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-28 22:34:45 -0500

    Extracting out assertions for specific results

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -13,11 +13,16 @@ describe('Search', function() {
     });
 
     describe('GET /seach', function() {
-        it('returns status code 200', function() {
-            var url = baseUrl + '/search?city=' + city;
+        var url = baseUrl + '/search?city=' + city;
+
+        it('should return status code 200', function() {
             request.get(url, function(error, response, body) {
                 expect(response.statusCode).toBe(200);
+            });
+        });
 
+        it('should return results', function() {
+            request.get(url, function(error, response, body) {
                 // It Should....
                 //
                 // return id
