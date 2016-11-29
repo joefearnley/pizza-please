ab096f31eb7c12a8b5902504442159cbef466a42
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-29 18:46:07 +0000

    Attempting to mock the main service. No dice

--- js/tests/app.spec.js
+++ js/tests/app.spec.js
@@ -13,15 +13,23 @@ describe('Pizza Please App Test Suite', function() {
 
         beforeEach(angular.mock.module('pizzaPlease'));
 
-        beforeEach(angular.mock.inject(function(_$controller_, $rootScope) {
+        beforeEach(angular.mock.inject(function(_$controller_, $rootScope, _$httpBackend_) {
+            $httpBackend = _$httpBackend_;
+            
             scope = $rootScope.$new();
             scope.city = 'Norton Shores, MI';
             searchController = _$controller_('SearchController', { $scope: scope });
         }));
 
-        beforeEach(inject(function($injector) {
-            $httpBackend = $injector.get('$httpBackend');
-        }));
+        // beforeEach(inject(function($injector) {
+        //     $httpBackend = $injector.get('$httpBackend');
+        // }));
+        
+        afterEach(function() {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
 
         it('should exist', function() {
             expect(searchController).toBeDefined();
@@ -73,21 +81,29 @@ describe('Pizza Please App Test Suite', function() {
                                     "Muskegon, MI 49441"
                                 ]
                             }
-                        }
+                        }, {}, {}
                     ]
                 };
 
-                // $httpBackend.when('GET', '/search')
-                //     .respond(200, fakeResponse);
+                //$httpBackend.when('GET', '/search')
+                //    .respond(200, fakeResponse);
+                    
+
 
                 scope.findPizza();
 
+                expect(scope.isLoading).toBe(true);
+                expect(scope.resultsLoaded).toBe(false);
+
+                //$httpBackend.flush();
+
+
                 // should have locations
-                //expect(fakeResponse.locations).toBeDefined();
-                // expect(scope.locations.length).toBe(1);
+                //expect(scope.locations).toBeDefined();
+                //expect(scope.locations.length).toBe(3);
 
                 // should have a map
-                //expect(scope.locations.length).toBe(20);
+                //expect(scope.map).toBeDefined();
 
                 // should have markers
                 //expect(scope.map).toBeDefined();
@@ -100,51 +116,56 @@ describe('Pizza Please App Test Suite', function() {
     });
 });
 
-describe('Search Service', function() {
-    var LocationService;
-    var $httpBackend;
+// describe('Search Service', function() {
+//     var LocationService;
+//     var $httpBackend;
 
-    beforeEach(angular.mock.module('pizzaPlease'));
+//     beforeEach(angular.mock.module('pizzaPlease'));
 
-    beforeEach(inject(function(_SearchService_, $rootScope, _$httpBackend_) {
-        SearchService = _SearchService_;
-        $httpBackend = _$httpBackend_;
-    }));
+//     beforeEach(inject(function(_SearchService_, $rootScope, _$httpBackend_) {
+//         SearchService = _SearchService_;
+//         $httpBackend = _$httpBackend_;
+//     }));
+    
+//     afterEach(function() {
+//         $httpBackend.verifyNoOutstandingExpectation();
+//         $httpBackend.verifyNoOutstandingRequest();
+//     });
 
-    it('should exist', function() {
-        expect(SearchService).toBeDefined();
-    });
+//     it('should exist', function() {
+//         expect(SearchService).toBeDefined();
+//     });
 
-    describe('search', function() {
+//     describe('search', function() {
 
-        var fakeResponse = {
-            success: true,
-            error: null,
-            locations: [
-                {
-                    name: "Mr Scrib's Pizza",
-                    phone: "2317331857",
-                    location: {
-                        city: "Muskegon",
-                        display_address: [
-                            "3044 Henry St",
-                            "Muskegon, MI 49441"
-                        ]
-                    }
-                }
-            ]
-        };
+//         var fakeResponse = {
+//             success: true,
+//             error: null,
+//             locations: [
+//                 {
+//                     name: "Mr Scrib's Pizza",
+//                     phone: "2317331857",
+//                     location: {
+//                         city: "Muskegon",
+//                         display_address: [
+//                             "3044 Henry St",
+//                             "Muskegon, MI 49441"
+//                         ]
+//                     }
+//                 }
+//             ]
+//         };
 
-        $httpBackend.when('GET', '/search')
-            .respond(200, fakeResponse);
+//         $httpBackend.when('GET', '/search')
+//             .respond(200, fakeResponse);
 
-        it('should find locations', function() {
-            $httpBackend.flush();
+//         it('should find locations', function() {
+//             $httpBackend.flush();
 
-            locations = SearchService.search();
+//             locations = SearchService.search();
 
-            expect(location.length).toBe(1);
-        });
-    });
+//             expect(location.length).toBe(1);
+//         });
+//     });
 
-});
+// });
