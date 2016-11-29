e6e621f3c4c9c0bfb7cb0575b8ce4e7ce8682994
Joe Fearnley <joe.fearnley@gmail.com>
2016-11-28 23:04:06 -0500

    Attempting to mock the main api endpoint

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,5 +1,6 @@
 
 var request = require('request');
+var nock = require ('nock');
 var baseUrl = 'http://localhost:5000/';
 var city = 'Norton Shores, MI'
 
@@ -14,6 +15,27 @@ describe('Search', function() {
 
     describe('GET /seach', function() {
         var url = baseUrl + '/search?city=' + city;
+        var fakeResponse = {};
+
+        beforeEach(function() {
+            fakeResponse = {
+                success: true,
+                error: null,
+                locations: [
+                    {
+                        name: "Mr Scrib's Pizza",
+                        phone: "2317331857",
+                        location: {
+                            city: "Muskegon",
+                            display_address: [
+                                "3044 Henry St",
+                                "Muskegon, MI 49441"
+                            ]
+                        }
+                    }
+                ]
+            };
+        });
 
         it('should return status code 200', function() {
             request.get(url, function(error, response, body) {
@@ -21,12 +43,18 @@ describe('Search', function() {
             });
         });
 
-        it('should return results', function() {
+        it('should return correct results', function() {
+            nock(baseUrl)
+                .get('/search?city=' + city)
+                .reply(200, fakeResponse);
+
             request.get(url, function(error, response, body) {
+                console.log(response);
+                expect(response.success).toBe(true);
+
                 // It Should....
                 //
                 // return id
-                //
                 // return location name
                 //
                 // return location phone number
