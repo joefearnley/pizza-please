0bba37baa9265ffdb8c60bb96b42e6d2c8e94018
Joe Fearnley <joe.fearnley@gmail.com>
2017-01-05 21:21:20 +0000

    Getting all api tests to run properly

--- tests/backend/index.spec.js
+++ tests/backend/index.spec.js
@@ -1,10 +1,17 @@
-var request = require('request');
-var baseUrl = 'http://localhost:5000/';
+var baseUrl = 'http://localhost:5000';
+var nock = require('nock');
+var request = require('supertest')(baseUrl);
 
-describe('Test Index', function() {
-    it('should return a 200', function() {
-        request.get(baseUrl, function(error, response, body) {
-            expect(response.statusCode).toBe(200);
+describe('GET /', function() {
+    
+    beforeEach(function() {
+        nock(baseUrl).get('/').reply(200);
+    });
+
+    it('should return a 200', function(done) {
+        request.get('/').end(function (error, response) {
+            expect(response.status).toBe(200);
+            done();
         });
     });
 });
