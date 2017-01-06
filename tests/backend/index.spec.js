476f66080943417f713dfab94356d1307dd45871
Joe Fearnley <joe.fearnley@gmail.com>
2017-01-05 20:11:53 -0500

    Updating test

--- tests/backend/index.spec.js
+++ tests/backend/index.spec.js
@@ -2,9 +2,10 @@ var request = require('request');
 var baseUrl = 'http://localhost:5000/';
 
 describe('Test Index', function() {
-    it('should return a 200', function() {
+    it('should return a 200', function(done) {
         request.get(baseUrl, function(error, response, body) {
             expect(response.statusCode).toBe(200);
+            done();
         });
     });
 });
