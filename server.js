f71bb050b26d42bf6b836eab3f414cc464ed69d3
Joe Fearnley <joe.fearnley@gmail.com>
2017-07-17 10:32:40 -0400

    converting to ES6

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 
-var express = require('express');
-var app = express();
-var request = require('request');
-var Yelp = require('yelp');
-var path = require('path');
+const express = require('express');
+const app = express();
+const request = require('request');
+const Yelp = require('yelp');
+const path = require('path');
 
 require('dotenv').config();
 
-app.use(function(req, res, next) {
+app.use( (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -16,25 +16,25 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-var yelp = new Yelp({
+const yelp = new Yelp({
     consumer_key: process.env.YELP_CONSUMER_KEY,
     consumer_secret: process.env.YELP_CONSUMER_SECRET,
     token: process.env.YELP_TOKEN,
     token_secret: process.env.YELP_TOKEN_SECRET,
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.get('/search', function (req, res) {
-    var results = {
+app.get('/search', (req, res) => {
+    let results = {
         success: true,
         error: null,
         locations: []
     };
 
-    var params = {
+    const params = {
         term: 'pizza'
     };
 
@@ -44,21 +44,19 @@ app.get('/search', function (req, res) {
         params.ll = req.query.ll;
     }
 
-    yelp.search(params).then(function (response) {
-        var businesses = response.businesses;
-
-        for (var i = 0; i < businesses.length; i++) {
-            results.locations.push(businesses[i]);
+    yelp.search(params).then( response => {
+        for (let i = 0; i < response.businesses.length; i++) {
+            results.locations.push(response.businesses[i]);
         }
 
         res.send(results);
-    }).catch(function (err) {
+    }).catch( err => {
         results.success = true,
         results.error = error;
         res.send(results);
     });
 });
 
-app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Example app listening on port 5000!');
 });
